80131d157c79a6afda9bc094d1dd1e3f71d08ea5
J-SangWon <124190818+J-SangWon@users.noreply.github.com>
2025-03-10 10:52:19 +0900

    study

---
+++ My project/Assets/ConditionExample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ConditionExample : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    public int health = 100;
+    // Update is called once per frame
+    void Update()
+    {
+        health -= 1;
+        if(health <= 0)
+        {
+            Debug.Log("체력이 없음");
+            health = 0;
+        }
+    }
+}
---
+++ My project/Assets/VariableExample.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class VariableExample : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log($"PlayerName : {playerName}");
+        Debug.Log($"PlayerScore : {PlayerScore}");
+        Debug.Log($"speed : {speed}");
+        Debug.Log($"isGameOver : {isGameOver}");
+    }
+
+    public string playerName = "Hero";
+    public int PlayerScore = 0;
+    public float speed = 5.5f;
+    public bool isGameOver = false;
+
+
+
+}
