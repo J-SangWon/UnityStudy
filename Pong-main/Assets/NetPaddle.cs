964d8ac1f0241476c2742e5035ca1482819f0347
J-SangWon <124190818+J-SangWon@users.noreply.github.com>
2025-05-02 18:15:15 +0900

    공부

---
+++ Pong-main/Assets/NetPaddle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class NetPaddle : MonoBehaviourPun
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+
+        //내가 소유한 오브젝트인지 확인하는 조건문이다.
+        if (photonView.IsMine)
+        {
+            float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+            transform.Translate(0, move, 0);
+        }
+    }
+}
